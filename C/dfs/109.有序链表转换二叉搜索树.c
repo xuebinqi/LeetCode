5aefefd6939097eee21e17ff4450a3cf7ec4faee
Xuebinqi <xuebinqi@github.com>
2020-12-12 11:04:01 +0800

    The Solution of 109

---
+++ C/dfs/109.有序链表转换二叉搜索树.c
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=109 lang=c
+ *
+ * [109] 有序链表转换二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct ListNode* getMedian(struct ListNode* left, struct ListNode* right) {
+    struct ListNode* fast = left;
+    struct ListNode* slow = left;
+    while (fast != right && fast->next != right) {
+        fast = fast->next;
+        fast = fast->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+
+struct TreeNode* buildTree(struct ListNode* left, struct ListNode* right) {
+    if (left == right) {
+        return NULL;
+    }
+    struct ListNode* mid = getMedian(left, right);
+    struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    root->val = mid->val;
+    root->left = root->right = NULL;
+    root->left = buildTree(left, mid);
+    root->right = buildTree(mid->next, right);
+    return root;
+}
+
+struct TreeNode* sortedListToBST(struct ListNode* head) {
+    return buildTree(head, NULL);
+}
+
+// @lc code=end
+
